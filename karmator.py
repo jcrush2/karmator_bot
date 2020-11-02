97f4ce51d35a6033f7524d64cea3f36967b2e06e
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2020-11-02 19:23:49 +1000

    Update karmator.py

--- karmator.py
+++ karmator.py
@@ -541,9 +541,9 @@ def changing_karma_text(msg):
 	
 @bot.message_handler(content_types=['text'])	
 def send_text(msg):
-	if msg.text.lower() == 'играть':
 	if is_karma_abuse(msg):
 		return
+	if msg.text.lower() == 'играть':
 	random_karma = ("+1", "-1", "-2", "+2", "+3", "-3")
 	random_karma2 = random.choice(random_karma)
 	change_karma(msg.from_user, msg.chat, random_karma2)
