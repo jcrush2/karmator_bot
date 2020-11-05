9661d4cd6dbfa33a19b8d4ff4e7d74ea9eae87b2
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2020-11-05 15:23:13 +1000

    Update karmator.py

--- karmator.py
+++ karmator.py
@@ -609,9 +609,9 @@ def send_text(msg):
 			
 	if msg.text.lower() == 'админ':
 		admins = bot.get_chat_administrators(-1001110839896)
-		admins2 = "222"
+		admins2 = ""
 		for admin in admins:
-			admins2 += f"*{admin.user.id}*.оооо\n"
+			admins2 += f"*{admin.user}*.оооо\n"
 			bot.reply_to(msg, admins2, parse_mode="HTML")
 
 	
