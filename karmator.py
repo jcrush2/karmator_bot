77c43daa0f5f068a175afa68274bc2867e375e37
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2020-11-08 19:10:32 +1000

    Update karmator.py

--- karmator.py
+++ karmator.py
@@ -348,7 +348,7 @@ def tinder(msg):
 		
 	if not selected_user:
 		top_mess = "Никто еще не заслужил быть в этом списке."
-	random_tinder = random.choice(user)
+	random_tinder = random.choice(len(user))
 	bot.send_message(msg.chat.id, random_tinder, parse_mode="HTML")
 
 
