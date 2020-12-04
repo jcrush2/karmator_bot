08912ac9b799630cc8b4451a6e1865c4474c5636
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2020-12-04 11:18:03 +1000

    Update karmator.py

--- karmator.py
+++ karmator.py
@@ -381,12 +381,12 @@ def top_bad(msg):
 
 		if user.user_name:
 			name = user.user_name.strip()
-			nameid = user.strip()
+			nameid = user.split(',')[0]
 		else:
 			name = user.user_nick.strip()
 		if name == "Telegram" or name == "ХабКарма":
 			name =""
-		top_mess += f"*{i+1}*. {name} {nameid.split(',')[0]}, ({user.karma})\n"
+		top_mess += f"*{i+1}*. {name} {nameid}, ({user.karma})\n"
 	if not selected_user:
 		top_mess = "Никто еще не заслужил быть в этом списке."
 	bot.send_message(msg.chat.id, top_mess, parse_mode="Markdown")
