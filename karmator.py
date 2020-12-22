128e2d7d1ac08e326d3a63a09fad42bfba702a09
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2020-12-22 17:47:24 +1000

    Update karmator.py

--- karmator.py
+++ karmator.py
@@ -669,8 +669,8 @@ def commands(msg, text):
 	
 	if msg.text.lower() in ['язабан']:
 		if msg.reply_to_message:
-			bot.send_message(msg.chat.id, f"{msg.from_user.first_name} <a href='tg://user?id=55910350'>❤</a>dfsd", parse_mode="HTML")
-			bot.send_poll(msg.chat.id, f'{msg.reply_to_message.from_user.first_name} Это опрос?', ['Да', 'Нет', 'Не знаю'],is_anonymous=False)
+			bot.send_message(msg.chat.id, f"<a href='tg://user?id=55910350'>🔫</a> <b>{msg.from_user.first_name}</b> предлагает выгнать <b>{msg.reply_to_message.from_user.first_name}<b> из Хабчата!", parse_mode="HTML")
+			bot.send_poll(msg.chat.id, f'Согласны выгнать {msg.reply_to_message.from_user.first_name} из Чата?', ['Да', 'Нет', 'Не знаю'],is_anonymous=False)
 		else:
 			return
 	
