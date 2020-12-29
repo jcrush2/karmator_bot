6009750b4ac84b29bfd923a28fa25e3fb341e53d
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2020-12-29 11:14:03 +1000

    Update karmator.py

--- karmator.py
+++ karmator.py
@@ -773,7 +773,7 @@ def query_handler(call):
 	if call.from_user.id == call.data.split()[2]:
 		bot.answer_callback_query(callback_query_id=call.id, show_alert=True,  text='собака')
 	else:
-		bot.reply_to(msg.reply_to_message,f"{call.from_user.id}---{call.data.split()[2]}", parse_mode="HTML")
+		bot.send_message(msg.chat.id,f"{call.from_user.id}---{call.data.split()[2]}", parse_mode="HTML")
   
 def reputation(msg, text):
 	""" TODO """
