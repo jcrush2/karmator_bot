df7c106b6611b865f03a672f56bd4ee96edc5869
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2020-12-29 11:55:08 +1000

    Update karmator.py

--- karmator.py
+++ karmator.py
@@ -770,7 +770,7 @@ def commands(msg, text):
 
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
-	if call.data == call.from_user.id:
+	if f"{call.data}" == f"{call.from_user.id}":
 		bot.answer_callback_query(callback_query_id=call.id, show_alert=True,  text='собака')
 	else:
 		bot.answer_callback_query(callback_query_id=call.id, show_alert=True,  text=f"{call.from_user.id}---{call.data}")
