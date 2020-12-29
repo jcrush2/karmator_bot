7f5fcf42c21fd837b5ebfc6cd0a91635b0e95c8b
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2020-12-29 11:39:53 +1000

    Update karmator.py

--- karmator.py
+++ karmator.py
@@ -763,17 +763,17 @@ def commands(msg, text):
 	if msg.text.lower() in ['крокодил']:
 
 		markup = telebot.types.InlineKeyboardMarkup()
-		button = telebot.types.InlineKeyboardButton(text='CLick me', callback_data=f'{msg.from_user.id}')
+		button = telebot.types.InlineKeyboardButton(text='CLick me', callback_data=msg.from_user.id)
 		markup.add(button)
 		bot.send_message(chat_id=msg.chat.id, text='Some text', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
-	if call.data==call.from_user.id:
+	if call.data in call.from_user.id:
 		bot.answer_callback_query(callback_query_id=call.id, show_alert=True,  text='собака')
 	else:
-		bot.answer_callback_query(callback_query_id=call.id, show_alert=True,  text=f"{call.from_user.id}---{call.data.split()[2]}")
+		bot.answer_callback_query(callback_query_id=call.id, show_alert=True,  text=f"{call.from_user.id}---{call.data}")
 		
   
 def reputation(msg, text):
