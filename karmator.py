4b62bed4fe38c8371bd63cb29c27c433a64760bc
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-01-02 11:51:08 +1000

    Update karmator.py

--- karmator.py
+++ karmator.py
@@ -317,10 +317,11 @@ def top_best(msg):
 				name = user.user_name.strip()
 			else:
 				name = user.user_nick.strip()
+			top_mess += f"{i+1}. <b>{name}</b> ({user.karma}) {user_rang}\n"
 		else:
 			user.karma==f"-{user.karma}"
-
-		top_mess += f"{i+1}. <b>{name}</b> ({user.karma}) {user_rang}\n"
+			
+			
 	if not selected_user:
 		top_mess = "Никто еще не заслужил быть в этом списке."
 	bot.send_message(msg.chat.id, top_mess, parse_mode="HTML")
