a8d07b18d81ee7c0c9a62b2e3025707ceb5244c2
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-01-07 18:04:37 +1000

    Update karmator.py

--- karmator.py
+++ karmator.py
@@ -274,8 +274,8 @@ def top_best(msg):
 	user_rang = "🤖 Бот"
 	top_mess = "📈 Топ благодаримых\n\n"
 	for i, user in enumerate(selected_user):
-		userstatus = bot.get_chat_member(msg.chat.id,user.userid)
-		if userstatus.status == 'creator' or userstatus.status == 'member' or userstatus.status == 'administrator':
+#		userstatus = bot.get_chat_member(msg.chat.id,user.userid)
+#		if userstatus.status == 'creator' or userstatus.status == 'member' or userstatus.status == 'administrator':
 			
 		
 			
