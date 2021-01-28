1a78a2b63aef0f3db41b34379c738bf684bf7b63
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-01-28 15:03:58 +1000

    Update karmator.py

--- karmator.py
+++ karmator.py
@@ -281,12 +281,11 @@ def top_best(msg):
 			name = user.user_name.strip()
 		else:
 			name = user.user_nick.strip()
-		
-		
+
 		try:
 
 			userstatus = bot.get_chat_member(msg.chat.id,user.userid)
-			if userstatus.status == 'creator' or userstatus.status == 'member' or userstatus.status == 'administrator':
+			if userstatus.status == 'creator' or userstatus.status == 'member' or userstatus.status == 'administrator' or userstatus.status != 'left':
 				if user.karma <= 9: user_rang = "🤖\n      <code>Бот</code>"
 				if 10 <= user.karma < 20: user_rang = "🤫\n      <code>Тихоня</code>"
 				if 20 <= user.karma < 30: user_rang = "🐛\n      <code>Личинка</code>"
@@ -323,8 +322,7 @@ def top_best(msg):
 			
 
 				top_mess += f"{i+1}. <b>{name}</b> ({user.karma}) {user_rang}\n"
-			if userstatus.status == 'left':
-				top_mess += f"{i+1}. <b>Вышел</b> (0) 🗑\n      <code>Покинул чат</code>\n"
+
 		except Exception:
 				top_mess += f"{i+1}. <b>Удаленный</b> (0) 🗑\n      <code>Покинул чат</code>\n"
 #				userstatus = bot.get_chat_member(msg.chat.id,user.userid)
