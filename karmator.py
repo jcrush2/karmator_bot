cbb953a922746d6a61c5604a7e40597242ab41a2
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-02-08 16:54:31 +1000

    Update karmator.py

--- karmator.py
+++ karmator.py
@@ -718,9 +718,10 @@ def commands(msg, text):
 	if msg.text.lower() in ['билет']:
 		bot.send_chat_action(msg.chat.id, "typing")
 		url = "https://api.travelpayouts.com/v1/prices/cheap"
-		querystring = {"origin":"MOW","destination":"HKT","depart_date":"2021-01"}
+		querystring = {"origin":"KHV","destination":"-","depart_date":"2021-01"}
 		headers = {'x-access-token': '83a5fe66f97a36e6f0be4b2be21a5552'}
 		response = requests.request("GET", url, headers=headers, params=querystring)
+		bot.reply_to(msg, f"📍 Цитата: {json['price'][0]}", parse_mode="HTML")
 		bot.reply_to(msg, f"📍 Цитата: {response.text}", parse_mode="HTML")
 		
 	if msg.text.lower() in ['купить']:
