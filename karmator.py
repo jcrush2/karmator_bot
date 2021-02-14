6ba68670d6346b6ecc5da1664dc7a1bb1107064e
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-02-14 11:07:43 +1000

    Update karmator.py

--- karmator.py
+++ karmator.py
@@ -1,9 +1,4 @@
 #!usr/bin/python3
-import speech_recognition as sr
-import soundfile as sf
-import urllib.request
-import subprocess
-
 import datetime
 import hashlib
 import string
@@ -30,51 +25,6 @@ saves_database = {}
 database="dss4fgfd"
 saves_database_id = {}
 database_id="111111"
-
-
-
-
-s = requests.session()
-
-def wav2text(dest_filename, id, file_name):
-    r = sr.Recognizer()
-    message = sr.AudioFile(dest_filename)
-
-    with message as source:
-        audio = r.record(source)
-    try:
-        result = r.recognize_google(audio, language="ru_RU")
-        bot.send_message(id, format(result))
-        os.remove(dest_filename)
-        os.remove(file_name)
-
-    except sr.UnknownValueError:
-        bot.send_message(id, 'Не удалось распознать текст')
-        os.remove(dest_filename)
-        os.remove(file_name)
-
-def oga2wav(file_name, id):
-    src_filename = file_name
-    dest_filename = file_name + '.wav'
-    process = subprocess.run(['ffmpeg', '-i', src_filename, dest_filename])
-    wav2text(dest_filename, id, file_name)
-
-def donwload(file_path, file_id, id):
-    url = f'https://api.telegram.org/file/bot{TELEGRAM_API}/' + file_path
-    urllib.request.urlretrieve(url, file_id + '.oga')
-    file_name = file_id + '.oga'
-    oga2wav(file_name, id)
-
-def request2text(file_id, id):
-    r = s.get(f'https://api.telegram.org/bot{TELEGRAM_API}/getFile?file_id=' + file_id)
-    r = json.loads(r.text)
-
-    donwload(r['result']['file_path'], r['result']['file_id'], id)
-
-
-@bot.message_handler(content_types=['voice'])
-def start_message(message):
-    request2text(message.voice.file_id, message.chat.id)
 
 def is_my_message(msg):
 	"""
