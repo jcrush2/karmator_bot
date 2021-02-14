d42af9c16f35af881f94d1a98f127d2f77da8cb6
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-02-15 00:29:19 +1000

    Update karmator.py

--- karmator.py
+++ karmator.py
@@ -733,30 +733,42 @@ def commands(msg, text):
 			BKK = data['data']['BKK']['1']['price']
 			BKK2 = data['data']['BKK']['1']['departure_at']
 			send_bilet+=f"✈️ Бангкок (Таиланд), цена: {BKK}, вылет: {BKK2}\n\n"
-		except KeyError:
-  			print("Key error. Do something else")
 		except Exception:
 			 print("Some other error")
 		try:
 			HKG = data['data']['HKG']['1']['price']
 			HKG2 = data['data']['HKG']['1']['departure_at']
 			send_bilet+=f"✈️ Гонконг (Китай), цена: {HKG}, вылет: {HKG2}\n\n"
-		except KeyError:
-  			print("Key error. Do something else")
 		except Exception:
 			 print("Some other error")
 		try:
 			NHA = data['data']['NHA']['1']['price']
 			NHA2 = data['data']['NHA']['1']['departure_at']
 			send_bilet+=f"✈️ Нячанг (Вьетнам), цена: {NHA}, вылет: {NHA2}\n\n"
-		except KeyError:
-  			print("Key error. Do something else")
 		except Exception:
 			 print("Some other error")
 		try:
 			AYT = data['data']['AYT']['1']['price']
 			AYT2 = data['data']['AYT']['1']['departure_at']
 			send_bilet+=f"✈️ Анталья (Турция), цена: {AYT}, вылет: {AYT2}\n\n"
+		except Exception:
+			 print("Some other error")
+		try:
+			BJS = data['data']['BJS']['1']['price']
+			BJS2 = data['data']['BJS']['1']['departure_at']
+			send_bilet+=f"✈️ Пекин (Китай), цена: {BJS}, вылет: {BJS2}\n\n"
+		except Exception:
+			 print("Some other error")
+		try:
+			CAN = data['data']['CAN']['1']['price']
+			CAN2 = data['data']['CAN']['1']['departure_at']
+			send_bilet+=f"✈️ Гуанчжоу (Китай), цена: {CAN}, вылет: {CAN2}\n\n"
+		except Exception:
+			 print("Some other error")
+		try:
+			CEB = data['data']['CEB']['1']['price']
+			CEB2 = data['data']['CEB']['1']['departure_at']
+			send_bilet+=f"✈️ Кебу (Филиппины), цена: {CEB}, вылет: {CEB2}\n\n"
 		except Exception:
 			 print("Some other error")
 
