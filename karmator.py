901a93c5f3611141e7bbddb1080729d47d1f55e6
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-02-17 15:47:35 +1000

    Update karmator.py

--- karmator.py
+++ karmator.py
@@ -968,8 +968,7 @@ def changing_karma_sticker(msg):
 	
 @bot.channel_post_handler(content_types=["text"])
 def channel_post(msg):
-	if 'Доброе утро' in msg.text.lower():
-		bot.forward_message(-1001110839896, msg.chat.id, msg.message_id)
+	bot.forward_message(-1001110839896, msg.chat.id, msg.message_id)
 
 @bot.message_handler(content_types=['text'])	
 def karma_game(msg):
